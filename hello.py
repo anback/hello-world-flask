696d692cc5f24beb7a1d35f195177eb4f12cbad0
anback <anders.back@me.com>
2017-12-19 16:10:07 +0100

    add better logging

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+import sys
+import logging
 from flask import Flask, render_template
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def hello_world():
